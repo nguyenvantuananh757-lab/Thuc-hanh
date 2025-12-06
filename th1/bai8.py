a0f428f2bc3b10710c236876af46d57843cb3dad
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-06 09:31:43 +0700

    Update bai8.py

--- th1/bai8.py
+++ th1/bai8.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 a,b=1,2
 total=0
 while (a<=4000000-1):
@@ -6,3 +11,4 @@ while (a<=4000000-1):
     a,b =b,a+b
     print(a,end=" " )
 print("\n sum of prime numbers term in fibonacci seriews:",total)
+
