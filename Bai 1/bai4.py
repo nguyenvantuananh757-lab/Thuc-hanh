d1566f2f55bb426a9f9ce38edda1562ed9603379
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:09:33 +0700

    Update bai4.py

--- Bai 1/bai4.py
+++ Bai 1/bai4.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 i=1;
 for j in range( 2,10):
   print("i:",i,"j:",j)
   print(i,"/",j)
   print(i/j);
+
