2269efb17a6a4d1cfcbae391c73a9ce0e71f3ce5
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:19:16 +0700

    Update cau6.py

--- Bai 3/bai3 python/cau6.py
+++ Bai 3/bai3 python/cau6.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 #cau 6
 full_name = input("Nhập họ và tên: ").strip().split()
 
@@ -6,3 +11,4 @@ ten = full_name[1]
 
 print("Họ:", ho)
 print("Tên:", ten)
+
