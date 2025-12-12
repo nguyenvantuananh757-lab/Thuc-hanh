4d73e7fd59ae3953c4f5486d0bf63de3c13aa226
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:11:54 +0700

    Refactor sum function and add student info prints

--- Bai 2/th2/2-2.py
+++ Bai 2/th2/2-2.py
@@ -1,4 +1,10 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 def sum(a,b):
  return a+b
 c=sum(4,5);
 print("tong cua 4 va 5="+str(c))
+
