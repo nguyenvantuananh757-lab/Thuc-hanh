c91aa6a334f35bdb97227240cdbb05ca00f21394
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:11:24 +0700

    Add student information to the script

--- Bai 2/th2/2-10.py
+++ Bai 2/th2/2-10.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 import math
 
 def Tinh(R):
@@ -38,3 +43,4 @@ try:
         
 except ValueError:
     print(f" Lỗi: Giá trị '{R_input}' không phải là một số hợp lệ.")
+
