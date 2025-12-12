50252a2f7ce95b322676ed0858d70752dc3e160e
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:08:05 +0700

    Update bai6.py

--- Bai 1/bai6.py
+++ Bai 1/bai6.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 j=[]
 for i in range(2000,3201):
     if (i%7==0)and (i%5!=0):
         j.append(str(i))
 print(','.join(j))
+
