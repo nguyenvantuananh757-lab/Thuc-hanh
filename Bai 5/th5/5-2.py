326567dd2b759f9fec03735cf78c77ecde6dc8a1
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:23:27 +0700

    Update 5-2.py

--- Bai 5/th5/5-2.py
+++ Bai 5/th5/5-2.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 import turtle, random
 colosrs=["red", "green", "blue", "orange", "purple", "pink", "yellow"]
 painter = turtle.Turtle()
@@ -9,3 +14,4 @@ for i in range (100):
   painter.right (30)
   painter.left (60)
   painter.setposition (0, 0)
+
