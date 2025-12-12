b4093af4d8cb9fd841dd1abe60fd92011f8c1174
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:16:20 +0700

    Update cau14.py

--- Bai 3/bai3 python/cau14.py
+++ Bai 3/bai3 python/cau14.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 #cau 14
 ds = input('Nhap chuoi: ').split()
 ds.sort()
 for ch in ds:
     print(ch)
+
