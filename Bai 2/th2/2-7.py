e68abf6cb19522632102f3106deeba2ddf3958cc
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:14:01 +0700

    Update 2-7.py

--- Bai 2/th2/2-7.py
+++ Bai 2/th2/2-7.py
@@ -1,6 +1,12 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 def checkValue(n):
     if n%2==0:
         print("đâY là 1 SỐ chẵn")
     else:
      print("đây là 1 số lẻ")
 checkValue(7)
+
