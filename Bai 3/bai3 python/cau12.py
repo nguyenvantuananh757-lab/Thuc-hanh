8eed8481a0a563277312d1d1684816dda6418c71
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:15:56 +0700

    Update cau12.py

--- Bai 3/bai3 python/cau12.py
+++ Bai 3/bai3 python/cau12.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 #cau 12
 ds = input('Nhap chuoi: ').split()
 ds.remove('123')
 for ch in ds:
     print(ch)
+
