70f46d8a1fa7daa9bcffae9987d325182ff186e1
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:09:20 +0700

    Update bai5.py

--- Bai 1/bai5.py
+++ Bai 1/bai5.py
@@ -1,4 +1,10 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 n=int(input("enter A number--->"));
 while n>=0:
           print (n);
           n=n-1;
+
