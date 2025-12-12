e995e78bf4b10e17f155428f3dac21b4ad9308e1
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:24:20 +0700

    Update 5-5-b.py

--- Bai 5/th5/5-5-b.py
+++ Bai 5/th5/5-5-b.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 import tkinter as tk
 
 root = tk.Tk()
@@ -44,3 +49,4 @@ for language, val in languages:
                    ).pack(anchor=tk.W, padx=10) 
                    
 root.mainloop()
+
