e84ac51981060c74fb5e8ba6fb4e9c8e0f5677fc
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:07:18 +0700

    Update bai9.py

--- Bai 1/bai9.py
+++ Bai 1/bai9.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 str = input("enter a string:")
 dict={}
 for n in str:
@@ -7,3 +12,4 @@ for n in str:
     else:
         dict[n]=1
         print(dict)
+
