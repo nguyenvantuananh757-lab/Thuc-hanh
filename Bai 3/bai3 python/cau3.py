ab806c27dc4426e9917b3e43dd4598409385cab5
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:18:37 +0700

    Update cau3.py

--- Bai 3/bai3 python/cau3.py
+++ Bai 3/bai3 python/cau3.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 #cau 3
 S = input("Nhap chuoi S: ")
 
 for ch in S:
     print(ch.upper())
+
