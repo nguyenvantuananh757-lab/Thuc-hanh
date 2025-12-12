680b3a3d5ee59634caca3bc489440f35ad96f3d7
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:21:36 +0700

    Update cau5.py

--- Bai 4/bai4 python/cau5.py
+++ Bai 4/bai4 python/cau5.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 #cau5
 class py_solution:
     def reverse_words(self, s):
@@ -5,3 +10,4 @@ class py_solution:
 
 # Ví dụ sử dụng
 print(py_solution().reverse_words('hello .py'))
+
