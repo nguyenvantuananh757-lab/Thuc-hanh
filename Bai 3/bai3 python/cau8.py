887e71147e6fd17b2da217378be85e2e3c4d8223
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:19:38 +0700

    Update cau8.py

--- Bai 3/bai3 python/cau8.py
+++ Bai 3/bai3 python/cau8.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 #cau 8
 words = input("Nhập các từ: ").split()
 
@@ -7,3 +12,4 @@ print("Các từ dài nhất:")
 for w in words:
     if len(w) == max_len:
         print(w)
+
