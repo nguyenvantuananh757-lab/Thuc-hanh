f263186777895256ab46e893822345e85689bb48
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:13:38 +0700

    Update 2-5.py

--- Bai 2/th2/2-5.py
+++ Bai 2/th2/2-5.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 a="hello guy"
 def say ():
     global a
@@ -5,3 +10,4 @@ def say ():
     print(a)
 say()
 print(a)
+
