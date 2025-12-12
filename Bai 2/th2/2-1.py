b7507685065d2544cdea27e3ab9964d2049caa90
nguyenvantuananh757-lab <nguyenvantuananh757@gmail.com>
2025-12-12 23:11:11 +0700

    Add student information and maintain sum function

--- Bai 2/th2/2-1.py
+++ Bai 2/th2/2-1.py
@@ -1,6 +1,12 @@
+print("sinh vien : nguyen van tuan anh")
+
+print("ma so sv :245751030110037")
+
+print("#############################")
 def sum(a,b):
     print ("sum="+str(a+b))
 #tinh tong 2 so 4,5
 sum(4,5)
 #tinh tong 2 so 3,7
 sum(3,7)
+
